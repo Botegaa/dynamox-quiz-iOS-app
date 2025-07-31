0f0905301eca3fba14fe4565a2dff6c801c0948d
Botegaa <botega123@hotmail.com>
2025-07-31 02:30:46 -0300

    Cria os elementos usados na HomeScreen

---
+++ DynamoxQuiz/Extensions/ThemeColors.swift
@@ -0,0 +1,14 @@
+//
+//  ThemeColors.swift
+//  DynamoxQuiz
+//
+//  Created by Pedro Botega on 31/07/25.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let quizMainColor = UIColor(red: 77/255, green: 9/255, blue: 41/255, alpha: 1.0)
+    static let backgroundGray = UIColor(red: 241/255, green: 241/255, blue: 241/255, alpha: 1.0)
+}
--- DynamoxQuiz/Views/Home/HomeScreen.swift
+++ DynamoxQuiz/Views/Home/HomeScreen.swift
@@ -5,7 +5,7 @@
 //  Created by Pedro Botega on 28/07/25.
 //
 import UIKit
-
+import SnapKit
 
 class HomeScreen: UIView {
     
@@ -14,6 +14,46 @@ class HomeScreen: UIView {
         img.translatesAutoresizingMaskIntoConstraints = false
         img.image = UIImage(named: "Logo")
         return img
+    }()
+    
+    lazy var logoLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Dynamox \nQuiz"
+        label.textAlignment = .center
+        label.font = UIFont(name: "Poppins-Bold", size: 28)
+        return label
+    }()
+    
+    lazy var nameTextFieldLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Digite seu nome;"
+        label.textAlignment = .center
+        label.font = UIFont(name: "Poppins-SemiBold", size: 16)
+        return label
+    }()
+    
+    lazy var nameTextFiel : UITextField = {
+        let tf = UITextField ()
+        tf.backgroundColor = .backgroundGray
+        tf.layer.borderColor = UIColor.quizMainColor.cgColor
+        tf.layer.cornerRadius = 12
+        return tf
+    }()
+    
+    lazy var startBttnView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor.quizMainColor
+        view.layer.cornerRadius = 12
+        return view
+    }()
+    
+    lazy var startBttn : UIButton = {
+        let bttn = UIButton ()
+        bttn.setTitle("Iniciar", for: .normal)
+        bttn.setTitleColor(UIColor.white ,for: .normal)
+        bttn.titleLabel?.font = UIFont(name: "Poppins-SemiBold", size: 18)
+        return bttn
     }()
 
     override init(frame: CGRect) {
