56e9a77bed469cba0a6ba8128386ff1b968c8a3d
Botegaa <botega123@hotmail.com>
2025-08-01 01:35:11 -0300

    Cria o model AnswerOption

---
+++ DynamoxQuiz/Models/AnswerOption.swift
@@ -0,0 +1,13 @@
+//
+//  AnswerOption.swift
+//  DynamoxQuiz
+//
+//  Created by Pedro Botega on 31/07/25.
+//
+
+import Foundation
+
+struct AnswerOption {
+    let id: Int
+    let text: String
+}
